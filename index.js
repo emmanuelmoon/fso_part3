597d4a2f8da7d9492d7340c4402280ef9d33c5a0
emmanuelmoon <emmenual123@gmail.com>
2023-06-25 21:07:14 +0500

    Completed 3.16

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const morgan = require("morgan");
 const cors = require("cors");
 const app = express();
 const Person = require("./models/phonebook");
-app.use(express.static("dist"));
+
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 morgan.token("body", (req) => {
   if (req.method === "POST") {
@@ -13,12 +16,23 @@ morgan.token("body", (req) => {
   }
 });
 
-app.use(express.json());
-app.use(
-  morgan(":method :url :status :res[content-length] - :response-time ms :body")
-);
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
 
 app.use(cors());
+app.use(express.json());
+app.use(express.static("dist"));
 
 app.get("/api/persons", (request, response) => {
   Person.find({}).then((persons) => {
@@ -40,30 +54,28 @@ app.get("/api/persons/:id", (request, response) => {
 });
 
 app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
-  console.log(persons);
-  response.status(204).end();
+  Person.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 app.post("/api/persons", (request, response) => {
   const body = request.body;
 
-  if (body.name === undefined) {
-    response.json({ error: "name not submitted" });
-  } else if (body.number === undefined) {
-    response.json({ error: "number not submitted" });
-  } else {
-    const person = new Person({
-      name: body.name,
-      number: body.number,
-    });
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  });
 
-    person.save().then((savedPerson) => {
-      response.json(savedPerson);
-    });
-  }
+  person.save().then((savedPerson) => {
+    response.json(savedPerson);
+  });
 });
+
+app.use(unknownEndpoint);
+app.use(errorHandler);
 
 const PORT = 3001;
 
